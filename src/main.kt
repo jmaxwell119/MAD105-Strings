d23a78a221b32418800f2e3b3bb4b36425a9c753
Jason Maxwell <jmaxwell119@students.mchenry.edu>
2020-09-27 15:18:18 -0500

    made guess a variable, used for all questions

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,6 @@
+// if i don't need to keep track of user input, i can use 1 variable
+// by running the if statement directly after asking the question
+
 fun main(){
     println("This is a sports quiz. You will be shown a number of positions that players might play, and you must type in the name of the sport that has those positions.")
     // store correct answers as constants
@@ -6,22 +9,14 @@ fun main(){
     val answer3 = "baseball"
     val answer4 = "hockey"
     val answer5 = "soccer"
-    // prompt user for an answer, use readLine to assign input to a constant
-    print("What sport has: point guard, power forward, center? ")
-    val guess1 = readLine()!!.toString()
-    print("What sport has: tight end, wide receiver, running back? ")
-    val guess2 = readLine()!!.toString()
-    print("What sport has: second baseman, catcher, right fielder? ")
-    val guess3 = readLine()!!.toString()
-    print("What sport has: left wing, goaltender, defenceman? ")
-    val guess4 = readLine()!!.toString()
-    print("What sport has: sweeper, midfielder, striker? ")
-    val guess5 = readLine()!!.toString()
     // counter to store number of correct responses
     var correctCounter = 0
 
+    // prompt user for an answer, use readLine to assign input to a constant
+    print("What sport has: point guard, power forward, center? ")
+    var guess = readLine()!!.toString()
     // use compareTo, with ignoreCase as true to compare guess with correct answer
-    val result1 = answer1.compareTo(guess1, true)
+    val result1 = answer1.compareTo(guess, true)
     // if two strings are the same, compareTo will result in 0. use if-else statement to print whether answer was correct
     if (result1 == 0){
         println("Question 1: Correct")
@@ -31,7 +26,9 @@ fun main(){
     else
         println("Question 1: Incorrect")
 
-    val result2 = answer2.compareTo(guess2, true)
+    print("What sport has: tight end, wide receiver, running back? ")
+    guess = readLine()!!.toString()
+    val result2 = answer2.compareTo(guess, true)
     if (result2 == 0){
         println("Question 2: Correct")
         correctCounter ++
@@ -39,7 +36,9 @@ fun main(){
     else
         println("Question 2: Incorrect")
 
-    val result3 = answer3.compareTo(guess3, true)
+    print("What sport has: second baseman, catcher, right fielder? ")
+    guess = readLine()!!.toString()
+    val result3 = answer3.compareTo(guess, true)
     if (result3 == 0){
         println("Question 3: Correct")
         correctCounter ++
@@ -47,7 +46,9 @@ fun main(){
     else
         println("Question 3: Incorrect")
 
-    val result4 = answer4.compareTo(guess4, true)
+    print("What sport has: left wing, goaltender, defenceman? ")
+    guess = readLine()!!.toString()
+    val result4 = answer4.compareTo(guess, true)
     if (result4 == 0){
         println("Question 4: Correct")
         correctCounter ++
@@ -55,7 +56,9 @@ fun main(){
     else
         println("Question 4: Incorrect")
 
-    val result5 = answer5.compareTo(guess5, true)
+    print("What sport has: sweeper, midfielder, striker? ")
+    guess = readLine()!!.toString()
+    val result5 = answer5.compareTo(guess, true)
     if (result5 == 0){
         println("Question 5: Correct")
         correctCounter ++
